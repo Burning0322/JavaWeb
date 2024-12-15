7c94e220297504d5505cc6823a2781134c677370
RENHONG <liurenhong@libiaorobot.com>
2024-12-15 20:54:17 +0800

    Init

---
+++ src/test/java/com/rh/webapplication/WebapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rh.webapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebapplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
