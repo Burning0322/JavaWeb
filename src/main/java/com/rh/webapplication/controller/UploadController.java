2df0234fc1c6159a49d3d0877d48319cd28bb883
RENHONG <liurenhong@libiaorobot.com>
2024-12-22 22:04:59 +0800

    Update all bug for AliyunOSSOperator

--- src/main/java/com/rh/webapplication/controller/UploadController.java
+++ src/main/java/com/rh/webapplication/controller/UploadController.java
@@ -2,6 +2,7 @@ package com.rh.webapplication.controller;
 
 import com.rh.webapplication.pojo.Result;
 import com.rh.webapplication.utils.AliyunOSSOperator;
+import com.rh.webapplication.utils.AliyunOSSProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
